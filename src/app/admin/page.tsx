cc5f101cc843e81c92b420c184539a32ac99e1ff
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-07-07 06:12:13 +0100

    feat: enhance AdminPage layout with structured sections for better organization

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,29 @@
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <div className="flex flex-col">
+      {/* Boxes Section */}
+      <section></section>
+
+      {/* Bottom Section */}
+      <section className="flex-1 flex bg-yellow-400 p-1">
+        <div className="bg-red-500">
+          {/* Card Section */}
+          <div>Card Management Section</div>
+
+          {/* Table Section */}
+          <div>Table Section</div>
+        </div>
+
+        <div className="bg-red-500">
+          {/* Card Section */}
+          <div>Pie Chart Section</div>
+
+          {/* Line Chart Section */}
+          <div>Line Chart Section</div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default AdminPage;
