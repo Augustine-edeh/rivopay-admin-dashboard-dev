5cb117347b90138366ebdf919aa321ea8d5397ce
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-07-16 03:07:54 +0100

    feat: dynamically update page-title for wallet page

--- src/app/admin/wallet/page.tsx
+++ src/app/admin/wallet/page.tsx
@@ -1,4 +1,13 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { usePageTitleStore } from "@/stores/ui/pageTitleStore";
+
 const WalletPage = () => {
+  const setTitle = usePageTitleStore((state) => state.setTitle);
+  useEffect(() => setTitle("Wallet!"), [setTitle]);
+
   return <div>Wallet Page</div>;
 };
 
