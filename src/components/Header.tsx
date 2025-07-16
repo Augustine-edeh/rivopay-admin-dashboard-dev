ccba89516e8275bb8c29ae0f31725c749388a0a1
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-07-16 02:46:47 +0100

    feat: refactor header to match new design scheme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,42 +1,68 @@
 "use client";
 
-import { Bell, Search } from "lucide-react";
+import { Bell, ChevronLeft, ChevronRight, Search } from "lucide-react";
 import UserAvatar from "./UserAvatar";
 import { Label } from "@/components/ui/label";
 import { Input } from "./ui/input";
 
 import { usePageTitleStore } from "@/stores/ui/pageTitleStore";
+import { Button } from "./ui/button";
 
 const Header = () => {
   const title = usePageTitleStore((state) => state.title);
 
   return (
-    <header className="h-16 flex items-center gap-28 bg-blue-600">
+    // <header className="h-16 flex items-center gap-28 bg-blue-600">
+    //   <h1 className="text-xl font-bold tracking-wide">{title}</h1>
+
+    //   <div className="flex-1 flex justify-between bg-blue-400">
+    //     <div className="relative hidden md:flex items-center bg-green-300 w-full max-w-2xl">
+    //       <Input
+    //         className="rounded-4xl pl-14 bg-dashboardLightGrayBG hover:bg-dashboardCardGray"
+    //         id="search"
+    //         placeholder="Search..."
+    //       />
+    //       <Label htmlFor="search" className="absolute left-3.5">
+    //         <Search className="text-separatorGray" />
+    //       </Label>
+    //     </div>
+
+    //     <div className="flex items-center ml-auto gap-10 bg-orange-400">
+    //       <div className="grid place-content-center bg-dashboardLightGrayBG hover:bg-dashboardCardGray rounded-full size-10">
+    //         <Bell />
+    //         <p className="sr-only">Notifications</p>
+    //       </div>
+
+    //       <div className="flex items-center gap-2.5 bg-dashboardLightGrayBG hover:bg-dashboardCardGray rounded-3xl px-4 py-1">
+    //         <UserAvatar />
+    //         <p>Admin</p>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </header>
+
+    <header className="h-16 flex justify-between items-center bg-blue-600">
       <h1 className="text-xl font-bold tracking-wide">{title}</h1>
 
-      <div className="flex-1 flex justify-between bg-blue-400">
-        <div className="relative hidden md:flex items-center bg-green-300 w-full max-w-2xl">
-          <Input
-            className="rounded-4xl pl-14 bg-dashboardLightGrayBG hover:bg-dashboardCardGray"
-            id="search"
-            placeholder="Search..."
-          />
-          <Label htmlFor="search" className="absolute left-3.5">
-            <Search className="text-separatorGray" />
-          </Label>
+      <div className="flex justify-center items-center gap-10 bg-dashboardLightGrayBG px-5 py-3 rounded-lg">
+        <div className="bg-transparent hover:bg-[#B3B3B35C] border border-separatorGray hover:border-transparent rounded-full transition-colors duration-150">
+          <ChevronLeft color="#737373" />
         </div>
+        <p>July 2025</p>
 
-        <div className="flex items-center ml-auto gap-10 bg-orange-400">
-          <div className="grid place-content-center bg-dashboardLightGrayBG hover:bg-dashboardCardGray rounded-full size-10">
-            <Bell />
-            <p className="sr-only">Notifications</p>
-          </div>
-
-          <div className="flex items-center gap-2.5 bg-dashboardLightGrayBG hover:bg-dashboardCardGray rounded-3xl px-4 py-1">
-            <UserAvatar />
-            <p>Admin</p>
-          </div>
+        <div className="bg-transparent hover:bg-[#B3B3B35C] border border-separatorGray hover:border-transparent rounded-full transition-colors duration-150">
+          <ChevronRight />
         </div>
+      </div>
+
+      <div className="relative flex items-center bg-red-500 dashboardLightGrayBG hover:bg-dashboardCardGray rounded-full">
+        <UserAvatar />
+        <Bell
+          fill="#BF2B2B"
+          color="#BF2B2B"
+          size={10}
+          className="absolute top-0 right-0"
+        />
       </div>
     </header>
   );
