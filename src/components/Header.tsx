328c2bd282883d32c24cac2e5b3d1f91a6fda9a1
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-07-17 07:35:48 +0100

    chore(refactor): remove date-selector UI in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,16 +18,6 @@ const Header = () => {
     <header className="h-16 flex justify-between items-center bg-blue-600">
       <h1 className="text-xl font-bold tracking-wide capitalize">{title}</h1>
 
-      <div className="flex justify-center items-center gap-10 bg-dashboardLightGrayBG px-5 py-3 rounded-lg">
-        <div className="hover:bg-[#B3B3B35C] border border-separatorGray hover:border-transparent rounded-full transition-colors duration-150">
-          <ChevronLeft color="#737373" />
-        </div>
-        <p>July 2025</p>
-        <div className="hover:bg-[#B3B3B35C] border border-separatorGray hover:border-transparent rounded-full transition-colors duration-150">
-          <ChevronRight />
-        </div>
-      </div>
-
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <div className="relative flex items-center cursor-pointer bg-dashboardLightGrayBG hover:bg-dashboardCardGray rounded-full p-1.5">
