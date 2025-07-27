c535b9b247d7ecc7222e172bdeedc64ebd585bf4
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-07-27 19:00:37 +0100

    feat: enforce stable styling for receipt component to match design specs

--- src/components/transactions/ReceiptModal.tsx
+++ src/components/transactions/ReceiptModal.tsx
@@ -31,11 +31,11 @@ const ReceiptModal = ({
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent
         showCloseButton={showCloseButton}
-        className="w-[360px] rounded-xl py-6"
+        className="flex flex-col justify-between w-[360px] h-[400px] px-0 rounded-t-[11px] rounded-b-none py-6 overflow-clip border-none shadow-none"
       >
-        <div className="text-center">
+        <div className="h-1/2 text-center">
           <p className="text-sm text-muted-foreground">
-            Bola Kuti Fawaz made a transaction of
+            Bola Tinunbu Fawaz made a transaction of
           </p>
           <h2 className="text-2xl font-bold my-2">{transaction.amount}</h2>
           <p className="text-sm text-muted-foreground">
@@ -43,33 +43,47 @@ const ReceiptModal = ({
           </p>
         </div>
 
-        <Separator className="my-4 border-dashed" />
-
-        <div className="flex items-center gap-3 border rounded-xl px-3 py-2">
-          <div className="bg-black text-white text-xs px-2 py-1 rounded-full">
-            rivo
-          </div>
-          <div className="text-sm">
-            <p className="font-medium">My Wallet card</p>
-            <p className="text-xs text-muted-foreground">
-              Reference: {transaction.ref}
-            </p>
-          </div>
-          <p className="ml-auto text-xs text-muted-foreground">
-            {transaction.time}
-          </p>
+        {/* receipt dash-divider */}
+        <div className="relative flex items-center">
+          <div className="absolute -left-5 size-8 rounded-full bg-black/50" />
+          <Separator className="flex-1 mx-7 my-4 bg-[#595959]/70 border border-dashed" />
+          <div className="absolute -right-5 size-8 rounded-full bg-black/50" />
         </div>
 
-        <p className="text-center text-xs text-green-600 mt-4">
-          on the {transaction.date}
-        </p>
+        {/* cut-out mark at the bottom of receipt */}
+        <div className="absolute -bottom-5 flex items-center">
+          <div className="size-9 rounded-full bg-black/50" />
+          <div className="size-9 rounded-full bg-black/50" />
+          <div className="size-9 rounded-full bg-black/50" />
+          <div className="size-9 rounded-full bg-black/50" />
+          <div className="size-9 rounded-full bg-black/50" />
+          <div className="size-9 rounded-full bg-black/50" />
+          <div className="size-9 rounded-full bg-black/50" />
+          <div className="size-9 rounded-full bg-black/50" />
+          <div className="size-9 rounded-full bg-black/50" />
+          <div className="size-9 rounded-full bg-black/50" />
+        </div>
 
-        <Button
-          className="mt-6 w-full rounded-full bg-[#DFDFDF] text-inherit hover:bg-textPurple hover:text-white h-8"
-          onClick={() => onOpenChange(false)}
-        >
-          Close
-        </Button>
+        <div className="h-1/2 px-6">
+          <div className="flex items-center gap-3 rounded-xl px-3 py-2 bg-dashboardAccentGray">
+            <div className="grid place-content-center size-12 bg-[#D9D9D9] text-xs px- rounded-[12px]">
+              rivo
+            </div>
+            <div>
+              <p className="font-bold">My Wallet card</p>
+              <p className="text-xs text-muted-foreground">
+                Reference: {transaction.ref}
+              </p>
+            </div>
+            <p className="ml-auto text-xs font-semibold text-muted-foreground">
+              {transaction.time}
+            </p>
+          </div>
+
+          <p className="text-center text-xs text-green-600 mt-4">
+            on the {transaction.date}
+          </p>
+        </div>
       </DialogContent>
     </Dialog>
   );
