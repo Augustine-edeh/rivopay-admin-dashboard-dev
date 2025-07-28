4daaff13d8e5799eae7bb8d559b38c9ed7c3a703
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-07-28 09:15:05 +0100

    feat:modify dashboard page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import { usePageTitleStore } from "@/stores/ui/pageTitleStore";
 import MetricCard from "@/components/MetricCard";
-import TrackCard from "@/components/TrackCard";
+// import TrackCard from "@/components/TrackCard";
 import PieChartComponent from "@/components/admin/PieChartComponent";
 import { ArrowDown, TrendingUp } from "lucide-react";
 
@@ -15,7 +15,8 @@ import {
   SelectItem,
 } from "@/components/ui/select";
 
-import SelectComponent from "@/components/admin/Select";
+// import SelectComponent from "@/components/admin/Select";
+import Image from "next/image";
 
 // import { metricsData } from "@/data/dashboardMetric";
 
@@ -52,6 +53,46 @@ const metricsData = [
     Icon: TrendingUp,
     iconColor: "#85B361",
     iconBgColor: "#00AB5733",
+  },
+];
+
+const summaryItems = [
+  {
+    title: "Total Users",
+    amount: 0,
+    description: "Users currently",
+    image: "/icons/total-users.svg",
+  },
+  {
+    title: "Total Merchants",
+    amount: 0,
+    description: "Merchants in service",
+    image: "/icons/total-users.svg",
+  },
+  {
+    title: "Card Holders",
+    amount: 0,
+    description: "Total card holders",
+    image: "/icons/total-taps.svg",
+  },
+  {
+    title: "Daily Taps",
+    amount: 0,
+    description: "Total number of Taps",
+    image: "/icons/total-taps.svg",
+  },
+  {
+    title: "Active Drivers",
+    amount: 0,
+    description: "Drivers currently in service",
+    image: "/icons/total-drivers.svg",
+  },
+
+  {
+    title: "Total Admins",
+    amount: 0,
+    description: "Active administrators",
+    image: "/icons/total-admins.svg",
   },
 ];
 
@@ -112,13 +153,46 @@ const DashboardPage = () => {
         </div>
 
         {/* Summary Cards Section */}
-        <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 flex-1">
-          <TrackCard title="Total Users" value={0} />
+        {/* NOTE: consider sm:grid-cols-3 */}
+        <div className="grid grid-cols-2 sm:grid-cols-2 gap-4 flex-1">
+          {summaryItems.map((summaryItem, index) => (
+            <div
+              key={index}
+              className="flex items-center gap-3.5 bg-dashboardAccentGray p-3.5 -[#FCFCFC] rounded-[10px]"
+            >
+              <Image
+                src={summaryItem.image}
+                alt="icon"
+                width={40}
+                height={40}
+              />
+
+              <div className="space-y-2 text-muted-foreground">
+                <h3 className="font-semibold text-black">
+                  {summaryItem.title}
+                </h3>
+                <p className="font-bold text-xl">{summaryItem.amount}</p>
+                <p className="text-xs">{summaryItem.description}</p>
+              </div>
+            </div>
+          ))}
+
+          {/* <div className="flex items-center gap-3.5 bg-[#FCFCFC] rounded-[10px]">
+            <Image src={`/summary1.svg`} alt="icon" width={40} height={40} />
+
+            <div className="space-y-2.5">
+              <h3>{`Title`}</h3>
+              <p>{`0`}</p>
+              <p>{`description`}</p>
+            </div>
+          </div> */}
+
+          {/* <TrackCard title="Total Users" value={0} />
           <TrackCard title="Total Merchants" value={0} />
           <TrackCard title="Card Holders" value={0} />
           <TrackCard title="DailyTaps" value={0} />
           <TrackCard title="Active Drivers" value={0} />
-          <TrackCard title="Total Admins" value={0} />
+          <TrackCard title="Total Admins" value={0} /> */}
         </div>
       </section>
     </section>
