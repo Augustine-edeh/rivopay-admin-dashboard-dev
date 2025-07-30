81fa587da045ffef01f83b10bd503a5ce5140498
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-07-30 21:01:17 +0100

    refactor(sidebar): extract logout button into a separate component for better modularity

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { Button } from "@/components/ui/button";
+import LogoutButton from "./side-bar/LogoutButton";
 
 const SideBar = () => {
   const pathname = usePathname();
@@ -130,31 +130,7 @@ const SideBar = () => {
       </nav>
 
       {/* Logout */}
-      <div className="border-t border-white/20 pt-6 px-4 mt-auto">
-        <Button
-          variant="ghost"
-          title="Log out"
-          className="group w-full justify-start gap-3 text-red-400 hover:bg-red-50 hover:text-red-700 focus:bg-red-50 focus:text-red-700 transition-colors"
-        >
-          <div className="relative size-5">
-            {/* Default logout icon */}
-            <Image
-              src="/icons/logout.svg"
-              alt="logout"
-              fill
-              className="transition-opacity duration-200 group-hover:opacity-0 group-focus:opacity-0 group-active:opacity-0"
-            />
-            {/* Active logout icon */}
-            <Image
-              src="/icons/logout-active.svg"
-              alt="logout active"
-              fill
-              className="opacity-0 transition-opacity duration-200 group-hover:opacity-100 group-focus:opacity-100 group-active:opacity-100"
-            />
-          </div>
-          Log out
-        </Button>
-      </div>
+      <LogoutButton />
     </aside>
   );
 };
