9c4ce9c02f17848dd7fa8dfec91887ee32a58486
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-08-03 20:10:26 +0100

    refactor(transactions): extract transactions array into separate file for easy separation of concerns

--- src/app/admin/transactions/page.tsx
+++ src/app/admin/transactions/page.tsx
@@ -7,65 +7,7 @@ import TransactionsTable from "@/components/transactions/TransactionsTable";
 import TransactionsPagination from "@/components/transactions/TransactionsPagination";
 import ReceiptModal from "@/components/transactions/ReceiptModal";
 import { TransactionsType } from "@/types/transactions/transactions";
-
-const mockTransactions: TransactionsType[] = [
-  {
-    ref: "#21223456216",
-    method: "Physical Card",
-    date: "12 Mar 2025",
-    time: "12:00pm",
-    amount: "₦300",
-    status: "Successful",
-  },
-  {
-    ref: "#21323456216",
-    method: "Virtual Card",
-    date: "12 Mar 2025",
-    time: "12:00pm",
-    amount: "₦300",
-    status: "Pending",
-  },
-  {
-    ref: "#21323456216",
-    method: "QR Code",
-    date: "12 Mar 2025",
-    time: "12:00pm",
-    amount: "₦500",
-    status: "Successful",
-  },
-  {
-    ref: "#21323456216",
-    method: "QR Code",
-    date: "12 Mar 2025",
-    time: "12:00pm",
-    amount: "₦300",
-    status: "Pending",
-  },
-  {
-    ref: "#21323456216",
-    method: "QR Code",
-    date: "12 Mar 2025",
-    time: "12:00pm",
-    amount: "₦500",
-    status: "Successful",
-  },
-  {
-    ref: "#21323456216",
-    method: "Physical Card",
-    date: "12 Mar 2025",
-    time: "12:00pm",
-    amount: "₦300",
-    status: "Pending",
-  },
-  {
-    ref: "#21323456216",
-    method: "Virtual Card",
-    date: "12 Mar 2025",
-    time: "12:00pm",
-    amount: "₦300",
-    status: "Successful",
-  },
-];
+import { transactions } from "@/data/transactions/transactions";
 
 const TransactionsPage = () => {
   const { setTitle } = usePageTitleStore((state) => state);
@@ -78,7 +20,7 @@ const TransactionsPage = () => {
     <section className="flex-1 flex flex-col gap-4 mt-10 mx-12 py-6 rounded-xl bg-dashboardAccentGray px-10">
       <TransactionsHeader />
       <TransactionsTable
-        transactions={mockTransactions}
+        transactions={transactions}
         onViewReceipt={(tx) => {
           setSelectedTx(tx);
           setModalOpen(true);
