061855785b93e9bd0aba54f0e74d8654b9f338c5
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-08-05 16:13:26 +0100

    chore(constants): extract sidebarLinks from SideBar component to constants/sidebarLinks.ts

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,74 +3,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { sidebarLinks } from "@/constants/sidebarLinks";
 
 import LogoutButton from "./side-bar/LogoutButton";
 
 const SideBar = () => {
   const pathname = usePathname();
-
-  const sidebarLinks = [
-    {
-      label: "Dashboard",
-      href: "/admin",
-      icon: "/icons/dashboard.svg",
-      activeIcon: "/icons/dashboard-active.svg",
-    },
-    {
-      label: "Wallet",
-      href: "/admin/wallet",
-      icon: "/icons/wallet.svg",
-      activeIcon: "/icons/wallet-active.svg",
-    },
-    {
-      label: "Assigned Cards",
-      href: "/admin/cards",
-      icon: "/icons/assigned-cards.svg",
-      activeIcon: "/icons/assigned-cards-active.svg",
-    },
-    {
-      label: "Transactions",
-      href: "/admin/transactions",
-      icon: "/icons/transactions.svg",
-      activeIcon: "/icons/transactions-active.svg",
-    },
-    {
-      label: "Merchants",
-      href: "/admin/merchants",
-      icon: "/icons/merchants.svg",
-      activeIcon: "/icons/merchants-active.svg",
-    },
-    {
-      label: "Merchants Request",
-      href: "/admin/merchants/request",
-      icon: "/icons/merchant-request.svg",
-      activeIcon: "/icons/merchant-request-active.svg",
-    },
-    {
-      label: "Drivers",
-      href: "/admin/drivers",
-      icon: "/icons/drivers.svg",
-      activeIcon: "/icons/drivers-active.svg",
-    },
-    {
-      label: "Analytics",
-      href: "/admin/analytics",
-      icon: "/icons/analytics.svg",
-      activeIcon: "/icons/analytics-active.svg",
-    },
-    {
-      label: "Settings",
-      href: "/admin/settings",
-      icon: "/icons/settings.svg",
-      activeIcon: "/icons/settings-active.svg",
-    },
-    {
-      label: "Help and Support",
-      href: "/admin/help",
-      icon: "/icons/help-and-support.svg",
-      activeIcon: "/icons/help-and-support-active.svg",
-    },
-  ];
 
   return (
     <aside className="hidden md:flex w-1/6 flex-col bg-darkPurple py-6 text-white space-y-10">
